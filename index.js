a1deb2dd53c4dbcd95e4d96a7ff8b43e8a6a6f7e
Diana Tran <diana.tran44@gmail.com>
2023-12-13 22:43:06 +1100

    updated JS

---
+++ index.js
@@ -0,0 +1,114 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { start } = require('repl');
+
+const prompt = inquirer.createPromptModule();
+
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+// Function to generate the README content based on user input
+function generateREADME(answers) {
+  return `
+# ${answers.projectTitle}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For questions about this project, please reach out to [${answers.githubUsername}](https://github.com/${answers.githubUsername}) or email at ${answers.email}.
+`;
+}
+
+// Function to write to the README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`File ${fileName} has been created successfully.`);
+  });
+}
+
+// Function to initialize the application
+function init() {
+  prompt(questions).then((answers) => {
+    const readmeContent = generateREADME(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
+
+// Initialize the application
+init();
